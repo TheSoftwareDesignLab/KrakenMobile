36d2561fa8c1cb3f64056e2c9cb6024920636772
William Ravelo <drummerwilliam@gmail.com>
2019-03-09 18:46:25 -0500

    mock implementation for read and write signal

--- lib/kraken-mobile/operations.rb
+++ lib/kraken-mobile/operations.rb
@@ -1,5 +1,11 @@
 module KrakenMobile
   module Operations
+    def readSignal(channel, timeout)
+      sleep(timeout)
+    end
 
+    def writeSignal(channel, content)
+      puts "Sending signal CONTENT:#{content} to CHANNEL:#{channel}"
+    end
   end
 end
--- lib/kraken-mobile/steps/communication_steps.rb
+++ lib/kraken-mobile/steps/communication_steps.rb
@@ -1,7 +1,7 @@
 Then /^I wait for a signal$/ do
-  sleep(10)
+  readSignal("algo", 10)
 end
 
 Then /^I send a signal$/ do
-  sleep(2)
+  writeSignal("algo", "Posiasfljaf")
 end
