70e345d921abda3ffc511792d2446013a188afe3
William Ravelo <drummerwilliam@gmail.com>
2019-04-06 16:45:25 -0500

    user can specify timeout

--- lib/kraken-mobile/runners/calabash/android/steps/communication_steps.rb
+++ lib/kraken-mobile/runners/calabash/android/steps/communication_steps.rb
@@ -3,6 +3,11 @@ Then("I wait for a signal containing {string}") do |string|
   readSignal(channel, string, KrakenMobile::Constants::DEFAULT_TIMEOUT)
 end
 
+Then("I wait for a signal containing {string} for {int} seconds") do |string, int|
+  channel = @scenario_tags.grep(/@user/).first
+  readSignal(channel, string, int)
+end
+
 Then("I send a signal to user {int} containing {string}") do |int, string|
   writeSignal("@user#{int}", string)
 end
