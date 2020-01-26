d8690cb9687a032a0f5f340f5874c794797dddc2
William Ravelo M <drummerwilliam@gmail.com>
2020-01-26 11:28:19 -0500

    added lifecycle

---
+++ lib/kraken-mobile/hooks/operations.rb
@@ -0,0 +1,10 @@
+require 'calabash-android/management/adb'
+require 'calabash-android/operations'
+
+def start_test_kraken_server_in_backgrouond
+  start_test_server_in_background
+end
+
+def shutdown_test_kraken_server
+  shutdown_test_server
+end
