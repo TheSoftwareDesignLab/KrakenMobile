d501ecd701b2b9953f1c3423b46c0e06742fdcc7
William Ravelo M <drummerwilliam@gmail.com>
2020-01-26 14:18:34 -0500

    fix

--- lib/kraken-mobile/test_scenario.rb
+++ lib/kraken-mobile/test_scenario.rb
@@ -57,6 +57,7 @@ class TestScenario
     end
   end
 
+  # TODO, take into account disconnected devices
   def self.ready_to_start?
     process_ids = DeviceProcess.registered_process_ids
     processes_ready = DeviceProcess.processes_in_state(
@@ -67,6 +68,7 @@ class TestScenario
     end
   end
 
+  # TODO, take into account disconnected devices
   def self.ready_to_finish?
     process_ids = DeviceProcess.registered_process_ids
     processes_finished = DeviceProcess.processes_in_state(
